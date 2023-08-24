d7755f94ad01463887c39153a64620252c788c2b
Marius Alban <malban298@gmail.com>
2023-08-24 18:37:21 -0300

    fuck

--- index.js
+++ index.js
@@ -2,25 +2,62 @@
 
 const template = document.createElement("div");
 
-const configurationObject = {
-  method: `POST`,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  body: JSON.stringify({
-    dogName: "Byron",
-    dogBreed: "Poodle",
-  }),
-};
-fetch("http://localhost:3000/dogs", configurationObject);
+document.addEventListener("DOMContentLoaded", submitData);
+
+//fetch("http://localhost:3000/dogs", configurationObject);
 
 function submitData(userName, userEmail) {
-  return fetch("http://localhost:3000/users", {
-    method: "POST",
+  let formData = {
+    name: userName,
+    email: userEmail,
+  };
+  const configurationObject = {
+    method: `POST`,
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.id);
+      JSON.stringify(data.id);
+    });
+}
+
+//  .catch(function (error) {
+//   alert("Unauthorized Access");
+//     console.log(error.message);
+//   });
+
+/*   .then((data) => {
+      fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          data: data.id,
+        }),
+      });
+    })
+    .then((res) => res.json());
+}
+
+/*
+
+//data.id;
+//.then((res) => res.json())
+//.then((res) => JSON.stringify(res));
+
+/*function updateData(userName, userEmail) {
+  return fetch(`http://localhost:3000/users/${(userName, userEmail)}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       name: userName,
@@ -28,19 +65,20 @@ function submitData(userName, userEmail) {
     }),
   })
     .then((res) => res.json())
-    .then(cb(userName, userEmail));
+    .then(data);
 }
 
-function initialize() {}
+// function initialize() {}
 
 function cb(x, y) {
-  const element = document.querySelector("body");
-  document.createElement("p");
-  element.appendChild("p");
-  element.appendChild("p");
-  const p1 = element.children[0];
-  const p2 = element.children[1];
-  p1.innerText = x;
-  p2.innerText = y;
+  const element = document.querySelector("tbody");
+  document.createElement("tr");
+  element.appendChild("tr");
+  element.appendChild("tr");
+  const tr1 = element.children[0];
+  const tr2 = element.children[1];
+  tr.innerText = x;
+  tr.innerText = y;
   return cb;
 }
+*/
