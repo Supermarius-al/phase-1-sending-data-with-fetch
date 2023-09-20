de93d6df5551824542f01321cd235918409d3f56
Marius Alban <malban298@gmail.com>
2023-09-20 12:54:26 -0300

    fuck canva

--- index.js
+++ index.js
@@ -19,21 +19,17 @@ function submitData(userName, userEmail) {
     },
     body: JSON.stringify(formData),
   };
-  return fetch("http://localhost:3000/users", configurationObject)
+ fetch("http://localhost:3000/users", configurationObject)
     .then((res) => res.json())
     .then((data) => {
       console.log(data.id);
       JSON.stringify(data.id);
     });
-}
-
-//  .catch(function (error) {
-//   alert("Unauthorized Access");
-//     console.log(error.message);
-//   });
-
-/*   .then((data) => {
-      fetch("http://localhost:3000/users", {
+}.catch(function (error) {
+alert("Unauthorized Access");
+    console.log(error.message);
+  }).then((data) => {
+      return fetch("http://localhost:3000/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -42,43 +38,12 @@ function submitData(userName, userEmail) {
         body: JSON.stringify({
           data: data.id,
         }),
-      });
+      })
     })
-    .then((res) => res.json());
-}
+    .then((res) => res.json())
 
 /*
-
-//data.id;
+data.id;
 //.then((res) => res.json())
 //.then((res) => JSON.stringify(res));
-
-/*function updateData(userName, userEmail) {
-  return fetch(`http://localhost:3000/users/${(userName, userEmail)}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: userName,
-      email: userEmail,
-    }),
-  })
-    .then((res) => res.json())
-    .then(data);
-}
-
-// function initialize() {}
-
-function cb(x, y) {
-  const element = document.querySelector("tbody");
-  document.createElement("tr");
-  element.appendChild("tr");
-  element.appendChild("tr");
-  const tr1 = element.children[0];
-  const tr2 = element.children[1];
-  tr.innerText = x;
-  tr.innerText = y;
-  return cb;
-}
 */
